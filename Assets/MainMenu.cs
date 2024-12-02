fe4558cb35f7454b1f9de6382d5c44b2e63b873c
Allen Wen <acwen11@gmail.com>
2024-12-01 22:52:41 -0500

    create main menu skeleton. Not yet linked to game execution

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public TextMeshProUGUI errorMsg;
+    public OptionsMenu options;
+
+    void Awake()
+    {
+        options.opt_set = false;        
+    }
+
+    public void StartGame()
+    {
+        if (!options.opt_set)
+        {
+            errorMsg.text = "Error: options not set.";
+            return;
+        }
+        else
+        {
+            Debug.Log("Starting Game.");
+            // SceneManager.LoadScene("Submarine");
+        }
+    }
+
+    public void GoToOptions()
+    {
+        errorMsg.text = string.Empty;
+        this.gameObject.SetActive(false);
+        options.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game.");
+        Application.Quit();
+    }
+}
---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using System.IO;
+using UnityEditor.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public TextMeshProUGUI isosurfCounter;
+    public TextMeshProUGUI errorMsg;
+    public MainMenu mainMenu;
+
+    [HideInInspector]
+    public bool opt_set;
+
+    void Awake()
+    {
+        opt_set = false;        
+    }
+
+    public void setSimName(string name)
+    {
+        PlayerPrefs.SetString("simname", name);
+    }
+
+    public void setCmap(int cmapid)
+    {
+        // TODO: define global enum?
+        switch (cmapid)
+        {
+            case 0:
+                PlayerPrefs.SetString("cmap", "inferno");
+                break;
+            case 1:
+                PlayerPrefs.SetString("cmap", "magma");
+                break;
+            case 2:
+                PlayerPrefs.SetString("cmap", "plasma");
+                break;
+            case 3:
+                PlayerPrefs.SetString("cmap", "viridis");
+                break;
+
+        }
+    }
+
+    public void setMin(string minstr)
+    {
+        float minval = float.Parse(minstr);
+        PlayerPrefs.SetFloat("min", minval);
+    }
+    public void setMax(string maxstr)
+    {
+        float maxval = float.Parse(maxstr);
+        PlayerPrefs.SetFloat("max", maxval);
+    }
+    public void setLog(bool log)
+    {
+        PlayerPrefs.SetInt("logscale", log ? 1 : 0);
+    }
+    
+    public void setNumSurf(float ns)
+    {
+        int intns = Mathf.RoundToInt(ns);
+        PlayerPrefs.SetInt("numSurfaces", intns);
+        isosurfCounter.text = ns.ToString();
+    }
+
+    public void TryBack()
+    {
+        // Check Options are Valid
+        float minval = PlayerPrefs.GetFloat("min");
+        float maxval = PlayerPrefs.GetFloat("max");
+        int logscale = PlayerPrefs.GetInt("logscale");
+
+        string sim_name = PlayerPrefs.GetString("simname");
+        string simdir = @"Assets/Gridfunctions/" + sim_name;
+        string simpar = @"Assets/Gridfunctions/" + sim_name + @"/" + sim_name + "_pars.txt";
+
+        if ((logscale == 1) && (minval <= 0))
+        {
+            errorMsg.text = "Error: minimum <= 0 when log scale is active.";
+            return;
+        }
+        else if (minval >= maxval)
+        {
+            errorMsg.text = "Error: minimum value >= maximum value.";
+            return;
+        }
+        /*
+        else if (!Directory.Exists(simdir))
+        {
+            errorMsg.text = "Error: data directory not found.";
+            return;
+        }
+        else if (!File.Exists(simpar))
+        {
+            errorMsg.text = "Error: chunk parameters not found.";
+            return;
+        }
+        */
+        else
+        {
+            opt_set = true;
+            errorMsg.text = string.Empty;
+            this.gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+            return;
+        }
+    }
+
+}
